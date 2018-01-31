dcf4f7aaa973fc0cd194d00c4845588440240410
Zheng Xiang <zxatbj@163.com>
2018-01-31 23:53:57 +0800

    Update docs and setup script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# auto_setup_ci
+Auto Setup CI
+=============
+
+A script setting up avocado-vt test environment.
+
+Simply run ./setup_avocado.sh to build and install avocado-vt.
+
--- setup_avocado.sh
+++ setup_avocado.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+# install requirements for avocado
 apt-get install -y python git gcc python-dev libvirt-dev \
         libffi-dev libssl-dev libyaml-dev xz-utils \
-        liblzma-dev
+        liblzma-dev make \
 
+# install newest python pip
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 
+# build and install avocado framework and optional plugins
 git clone https://github.com/avocado-framework/avocado.git
 
 cd avocado
@@ -14,12 +17,16 @@ python setup.py install
 make requirements-plugins
 make link
 
+# install avocado-vt plugin
 cd ..
 git clone https://github.com/avocado-framework/avocado-vt.git
 cd avocado-vt
 make requirements
 python setup.py install
 
-apt-get install -y arping tcpdump
+# install tools for avocado-vt
+apt-get install -y arping tcpdump fakeroot
 
+# init avocado-vt
 avocado vt-bootstrap
+
