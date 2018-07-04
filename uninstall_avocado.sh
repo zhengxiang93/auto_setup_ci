7c5a3427729c7ba43e2d50b3f6ec467c18468f28
zhengxiang93 <35922993+zhengxiang93@users.noreply.github.com>
2018-07-04 12:01:21 +0800

    Update uninstall_avocado.sh
    remove copied patches and directories.

--- uninstall_avocado.sh
+++ uninstall_avocado.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 pip uninstall -y avocado-framework avocado-framework-plugin-vt
+rm -rf ~/.local/lib/python2.7/site-packages/test-providers.d/
+rm -rf ~/.local/lib/python2.7/site-packages/shared/
+rm -rf ~/.local/lib/python2.7/site-packages/*.patch
+rm -rf ~/.local/lib/python2.7/site-packages/backends/
